4e617d1cc6c967d5a4feedac587c4e25f1a1a16e
Frank Jobst <fajobst@gmail.com>
2021-04-26 19:24:05 -0500

    master

--- src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SfgJmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SfgJmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SfgJmsApplication.class, args);
+    }
 
 }
