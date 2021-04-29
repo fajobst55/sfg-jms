73f04e491122bd886c6b99b77d826b4446cf8554
Frank Jobst <fajobst@gmail.com>
2021-04-28 20:38:25 -0500

    spring-message-type

--- src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -37,7 +37,8 @@ public class HelloMessageListener {
 
     @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
     public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
-                       @Headers MessageHeaders headers, Message message) throws JMSException {
+                       @Headers MessageHeaders headers, Message jmsMessage,
+                               org.springframework.messaging.Message springMessage) throws JMSException {
 
         HelloWorldMessage payloadMsg = HelloWorldMessage
                 .builder()
@@ -45,7 +46,11 @@ public class HelloMessageListener {
                 .message("World!!")
                 .build();
 
-        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
+        //example to use Spring Message type
+       // jmsTemplate.convertAndSend((Destination) springMessage.getHeaders().get("jms_replyTo"), "got it!");
+
+        jmsTemplate.convertAndSend(jmsMessage.getJMSReplyTo(), payloadMsg);
+
     }
 
 }
